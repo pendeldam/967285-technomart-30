d0a1e1336d18b73cac2a18f571d79923a282d7e6
pendeldam <45753426+pendeldam@users.noreply.github.com>
2021-02-25 00:45:05 +0300

    fixed promo-slider

--- js/main-page/slider.js
+++ js/main-page/slider.js
@@ -4,54 +4,79 @@ const promoSlider = document.querySelector(`.promo-slider`);
 const promoSliderNav = promoSlider.querySelector(`.promo-slider__nav`);
 const promoSliderNavBtns = promoSliderNav.querySelectorAll(`button`);
 const promoSliderControls = promoSlider.querySelector(`.promo-slider__controls`);
+const promoSliderControlsBtns = promoSliderControls.querySelectorAll(`button`);
+const slideCount = promoSlider.querySelectorAll(`.promo-slider__list-item`).length;
+
+const disableSlide = (number) => {
+  promoSlider.classList.remove(`promo-slider__slide-${number}`);
+
+  promoSlider
+    .querySelector(`.promo-slider__list-item:nth-child(${number})`)
+    .classList.remove(`promo-slider__list-item--active`);
+
+  promoSliderControls
+    .querySelector(`button:nth-child(${number})`)
+    .classList.remove(`promo-slider__controls--active`);
+};
+
+const enableSlide = (number) => {
+  promoSlider.classList.add(`promo-slider__slide-${number}`);
+
+  promoSlider
+    .querySelector(`.promo-slider__list-item:nth-child(${number})`)
+    .classList.add(`promo-slider__list-item--active`);
+
+  promoSliderControls
+    .querySelector(`button:nth-child(${number})`)
+    .classList.add(`promo-slider__controls--active`);
+};
+
+const changeSlide = (evt) => {
+  if (currentSlide === 1) {
+    disableSlide(currentSlide);
+
+    currentSlide = evt.target.dataset.id === `prev`
+      ? currentSlide = slideCount
+      : currentSlide = currentSlide + 1;
+
+    enableSlide(currentSlide);
+
+  } else if (currentSlide === slideCount) {
+    disableSlide(currentSlide);
+
+    currentSlide = evt.target.dataset.id === `prev`
+    ? currentSlide = currentSlide - 1
+      : currentSlide = 1;
+
+    enableSlide(currentSlide);
+
+  } else {
+    disableSlide(currentSlide);
+
+    currentSlide = evt.target.dataset.id === `prev`
+      ? currentSlide = currentSlide - 1
+      : currentSlide = currentSlide + 1;
+
+    enableSlide(currentSlide);
+  }
+};
 
 let currentSlide = 1;
 
 [...promoSliderNavBtns].forEach((btn) => {
-  btn.addEventListener(`click`, () => {
-    if (currentSlide > 1) {
-      promoSlider.classList.remove(`promo-slider__slide-${currentSlide}`);
+  btn.addEventListener(`click`, changeSlide);
+});
 
-      promoSlider
-        .querySelector(`.promo-slider__list-item:nth-child(${currentSlide})`)
-        .classList.remove(`promo-slider__list-item--active`);
+[...promoSliderControlsBtns].forEach((btn) => {
+  btn.addEventListener(`click`, (evt) => {
+    const number = +evt.target.dataset.id;
 
-      promoSliderControls
-        .querySelector(`button:nth-child(${currentSlide})`)
-        .classList.remove(`promo-slider__controls--active`);
-
-      currentSlide--;
-
-      promoSlider.classList.add(`promo-slider__slide-${currentSlide}`);
-
-      promoSlider
-        .querySelector(`.promo-slider__list-item:nth-child(${currentSlide})`)
-        .classList.add(`promo-slider__list-item--active`);
-
-      promoSliderControls
-        .querySelector(`button:nth-child(${currentSlide})`)
-        .classList.add(`promo-slider__controls--active`);
-
+    if (currentSlide === number) {
+      return;
     } else {
-      promoSlider.classList.remove(`promo-slider__slide-${currentSlide}`);
-      promoSlider
-        .querySelector(`.promo-slider__list-item:nth-child(${currentSlide})`)
-        .classList.remove(`promo-slider__list-item--active`);
-
-      promoSliderControls
-        .querySelector(`button:nth-child(${currentSlide})`)
-        .classList.remove(`promo-slider__controls--active`);
-
-      currentSlide = 2;
-
-      promoSlider.classList.add(`promo-slider__slide-${currentSlide}`);
-      promoSlider
-        .querySelector(`.promo-slider__list-item:nth-child(${currentSlide})`)
-        .classList.add(`promo-slider__list-item--active`);
-
-      promoSliderControls
-        .querySelector(`button:nth-child(${currentSlide})`)
-        .classList.add(`promo-slider__controls--active`);
+      disableSlide(currentSlide);
+      currentSlide = number;
+      enableSlide(currentSlide);
     }
   });
 });
